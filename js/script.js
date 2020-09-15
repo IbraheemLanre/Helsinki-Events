e5df4b37d5fa330617143326d9705d2fbba2a88d
IbraheemLanre <s2001108@edu.bc.fi>
2020-09-15 14:49:15 +0300

    modified script.js file

--- js/script.js
+++ js/script.js
@@ -8,6 +8,25 @@
     lat = position.coords.latitude;
     lon = position.coords.longitude;
 
+    // Initialize the platform object:
+    var platform = new H.service.Platform({
+      apikey: "CzTSptUZISX8B88Sa0jrEBK2BRA8K0B2pO_pe41ImGk",
+    });
+
+    // Obtain the default map types from the platform object
+    var maptypes = platform.createDefaultLayers();
+
+    // Instantiate (and display) a map object:
+    var map = new H.Map(
+      document.getElementById("mapContainer"),
+      defaultLayers.normal.map,
+      {
+        zoom: 10,
+        center: { lat: 52.5, lng: 13.4 },
+      }
+    );
+
+    console.log(map);
     fetch(`https://api.hel.fi/linkedevents/v1/place/?format=json`)
       .then(function (resp) {
         return resp.json();
